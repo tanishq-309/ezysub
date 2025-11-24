97e398314dfe828ccdc0196d3ca982aab4f6d38b
tanishq-309 <tanishqgoyal1001@gmail.com>
2025-11-24 14:46:12 +0530

    initial commit

---
+++ server/src/index.ts
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Server working");
+});
+
+app.listen(3000, () => {
+    console.log("Server running");
+});
